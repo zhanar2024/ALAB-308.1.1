c90b7f450eb8267e0357462b1da43f826d8a429b
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-05-21 14:29:45 -0400

    3rd commit

--- lab.js
+++ lab.js
@@ -8,3 +8,7 @@ const n4 = 5;
 const allDivisibleBy5 =
   n1 % 5 === 0 && n2 % 5 === 0 && n3 % 5 === 0 && n4 % 5 === 0;
 console.log("All numbers are divisible by 5: " + allDivisibleBy5);
+
+//Check if the first number is larger than the last
+const isLargerThanLast = n1 > n4;
+console.log("The first number is larger than the last: " + isLargerThanLast);
